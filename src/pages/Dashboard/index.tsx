062006a5aa8d79b0233438af4eb4a1302a37f897
alopsantos <anderson_lops@hotmail.com>
2020-08-04 00:28:19 -0300

    target=_blank

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -15,25 +15,25 @@ const Dashboard: React.FC = () => (
         <ul>
           <li>
             <FaInstagram size={24} />
-            <a href="https://www.instagram.com/thaisreis.nails/" title="">
+            <a href="https://www.instagram.com/thaisreis.nails/" target="_blank" title="Veja também meu perfil">
               Me
             </a>
           </li>
           <li>
             <FaCalendarAlt size={24} />
-            <a href="http://bit.ly/WAgendar" title="">
+            <a href="http://bit.ly/WAgendar" target="_blank" title="Link para agendamento atraves do whatsapp">
               Agendamentos
             </a>
           </li>
           <li>
             <FaFacebook size={24} />
-            <a href="https://www.facebook.com/thaisr.espaconails" title="">
+            <a href="https://www.facebook.com/thaisr.espaconails" target="_blank" title="Siga também no facebook">
               Facebook
             </a>
           </li>
           <li>
             <FaMapMarkerAlt size={24} />
-            <a href="https://www.google.com.br/maps/dir//-25.4437278,-54.4028833/@-25.4437629,-54.4029315,21z/data=!4m2!4m1!3e0" title="">
+            <a href="https://www.google.com.br/maps/dir//-25.4437278,-54.4028833/@-25.4437629,-54.4029315,21z/data=!4m2!4m1!3e0" target="_blank" title="Localização do meu espaço.">
               Localização
             </a>
           </li>
